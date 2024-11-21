2b4a4eac3106778df5e3a139bd4dfae44895a0e6
sreyareddy1902 <158560047+sreyareddy1902@users.noreply.github.com>
2024-11-21 15:33:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+
+//1.new Date()
+let currentDate=new Date()
+console.log(currentDate)
+
+//2.new Date(milliseconds)
+let currentDate1=new Date(5000)
+console.log(currentDate1)
+
+//3.new Date(date string)
+let currentDate2=new Date("23 jan 2023")
+console.log(currentDate2)
+
+let currentDate21=new Date("23 ,jan 2023")
+console.log(currentDate21)
+
+let currentDate22=new Date("23 jan 2024 12:30:45")
+console.log(currentDate22)
+
+
+//4.new Date(yyyy,month,date,hours,minutes,seconds,milliseconds)
+let currentDate3=new Date(2022,11,19,14,45,56,1000)
+console.log(currentDate3)
+
+
+//get methods
+
+let d1=new Date()
+console.log(d1)
+
+console.log(d1.getFullYear())
+console.log(d1.getMonth())
+console.log(d1.getDate())
+console.log(d1.getDay())
+console.log(d1.getHours())
+console.log(d1.getMinutes())
+console.log(d1.getSeconds())
+console.log(d1.getMilliseconds())
+console.log(d1.getTime())
+console.log(d1.getUTCFullYear())
+
+
+//set methods
+let d2=new Date()
+console.log(d2)
+
+let d21=d2.setFullYear(2020)
+console.log(d2)
+
+console.log(d2.setMonth(2))
+console.log(d2)
+
+console.log(d2.setDate(4))
+console.log(d2)
+
+console.log(d2.setHours(23))
+console.log(d2)
+
+console.log(d2.setMinutes(59))
+console.log(d2)
+
+console.log(d2.setSeconds(45))
+console.log(d2)
+
